ba6dc871ed2a31fce959b5b3daca5bca7a45292c
MiriamC18 <131303373+MiriamC18@users.noreply.github.com>
2023-10-14 14:02:21 -0600

    Add files via upload

---
+++ App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Integer a;
+        int b;
+        a=10;
+        b=20;
+        System.out.println(a+b);
+        int charInt = Integer.parseInt("66");
+        System.out.println(charInt);
+        String nombre= "Miriam";
+        System.out.println("hola + Miriam");
+
+    }
+}
